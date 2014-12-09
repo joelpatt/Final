edd62280e53e1a8e032a06d8084b2c283a6b3e3b
joel.patt15@gmail.com <Joel Patterson>
2014-12-09 14:08:46 -0500

    Final_project

--- main.rb
+++ main.rb
@@ -5,21 +5,64 @@ OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 get "/" do
     @title = "Joel Patterson's Portfolio"
     @description = "This site showcases all of the awesome things that Joel Patterson has done."
+    
+    require "sinatra"
+    require "twitter"
+    client = Twitter::REST::Client.new do |config|
+    config.consumer_key        = "X5tRK3AK2iWMSf542zMpXM4DC"
+    config.consumer_secret     = "1tGmuinA6KJqVKuWQwK7QNraZZKlsPdnxxzD87MEuTgoswq45j"
+    config.access_token        = "1875505560-kZNbBGn5OowWAA8yRfqiBh75O1FHbqz5TfHckhy"
+    config.access_token_secret = "0JU47mOnOw4VFx4pNZHallL2oyYTDbTXCtWZnUaaq8pPk"
+end
+    
+    @search_results = client.search("Affordable Care Act", result_type: "most retweeted").take(10).collect do |tweet|
+    #"{tweet.user.screen_name}: {tweet.text}"
+        tweet
+        
+    end
+       
+    require "sinatra"
+    require "instagram"
+
+    #enable :sessions
+
+    CALLBACK_URL = "http://localhost:4567/oauth/callback"
+
+    Instagram.configure do |config|
+        config.client_id = "e8cf79e549a0447d9ebc7947432023ae"
+        config.client_secret = "debb5ed5acbe48d3bb034fee1b2633b2"
+        # For secured endpoints only
+        #config.client_ips = '<Comma separated list of IPs>'
+    end
+    
+     @ig_results = []  
+    client = Instagram.client(:access_token => session[:access_token])
+  tags = client.tag_search('#CanadianTuxedo')
+  #html << "<h2>Tag Name = #{tags[0].name}. Media Count =  #{tags[0].media_count}. </h2><br/><br/>"
+  for media_item in client.tag_recent_media(tags[0].name)        
+      @ig_results.push(media_item) 
+   # html << "<img src='#{media_item.images.thumbnail.url}'>"
+  end   
+    
     erb :home
 end
 
-get '/about' do
-    @title = "About Me"
-    @description = " This page provides a short bio for Joel Patterson"
-    @about = "active"
-    erb :about
-end
 
-get '/works' do
-    @title = "My Clips"
-    @description = " This page provides links to Joel Patterson's published clips."
-    erb :work
-end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 get '/tweets' do
     require 'twitter'
@@ -30,7 +73,7 @@ get '/tweets' do
     config.access_token_secret = "0JU47mOnOw4VFx4pNZHallL2oyYTDbTXCtWZnUaaq8pPk"
 end
     
-    @search_results = client.search("#CanadianTuxedo", result_type: "recent").take(30).collect do |tweet|
+    @search_results = client.search("Affordable Care Act", result_type: "recent").take(30).collect do |tweet|
     #"#{tweet.user.screen_name}: #{tweet.text}"
         tweet
     end
@@ -40,7 +83,6 @@ end
     @tweets = "active"
     erb :tweets
 end
-
 
 
 get '/news' do
